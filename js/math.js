5b503b12b5082cb5f6777689edf2edb7e345ce3c
Daryl Mcintyre <darylmarkmcintyre@gmail.com>
2013-01-31 19:35:57 +0800

    Updated Math.js

--- js/math.js
+++ js/math.js
@@ -1,37 +1,43 @@
 function vector(a,b) {
+	//vector expressed as (i,j)
 	this.i = a;
 	this.j = b;
 	
 	//Adds a vector
 	this.add = function(v){
-		this.i += v.i;
-		this.j += v.j;
+		return new vector(this.i + v.i,	this.j + v.j);
 	}
 	//Adds a vector
 	this.addn = function(x,y){
-		this.i += x;
-		this.j += y;
+		return new vector(this.i + x,	this.j + y);
 	}
 	//Subtracts a vector
 	this.sub = function(v){
-		this.i -= v.i;
-		this.j -= v.j;
+		return new vector(this.i - v.i,	this.j - v.j);
 	}
 	//Subtracts a vector
 	this.subn = function(x,y){
-		this.i -= x;
-		this.j -= y;
+		return new vector(this.i - x,	this.j - y);
 	}
 	//Computes the dot product
 	this.dotProduct = function(v){
 		return this.i * v.j - this.j * v.i;
 	}
-	//Returns a norm
-	this.norm = function(){
-		var n = 1/Math.sqrt(this.i*this.i + this.j*this.j); 
-		//document.write(n);
-		var v = new vector(n*i,n*j);
-		//document.write("" + v.i + " " + v.j);
-		return v;
+	//Computes magnitude of the vector
+	this.magnitude = function(){
+		return Math.sqrt(this.i*this.i + this.j*this.j);
+	}
+	//Returns a normalised vector
+	this.normalise = function(){
+		var n = 1/this.magnitude();
+		return v = new vector(n*this.i,n*this.j);
+	}
+	//Returns a perpendicular vector
+	this.perp = function(){
+		return new vector(-this.j, this.i);
+	}
+	//Prints vector as a string
+	this.toString = function(){
+		return "Vector("+this.i+", "+this.j+")\n";
 	}
 }
